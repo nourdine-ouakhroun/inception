f071567be445639abb19a6d88fe348f111085435
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2023-10-01 19:23:33 +0100

    setup

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd goinfre
+mkdir docker
+rm -rf ~/Library/Containers/com.docker.docker
+ln -s ~/goinfre/docker ~/Library/Containers/com.docker.docker
+
+brew > brew_check 2>&1
+check=$(cat brew_check |  wc -l | tr -d " ")
+if [ $check -lt 5 ]
+then
+    path_to="/goinfre/$(whoami)/"
+    echo 'export path_to="/goinfre/$(whoami)/"' >> $HOME/.zshrc
+    git clone https://github.com/Homebrew/brew $path_to/.brew
+    echo 'export PATH=$path_to/.brew/bin:$PATH' >> $HOME/.zshrc
+    source $HOME/.zshrc
+    brew help
+fi
