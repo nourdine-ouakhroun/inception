b6cc1bb6259186c3532392655775f4b73836bd6b
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2023-10-02 23:27:03 +0100

    mariadb

---
+++ mariadb/mariadbconf copy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ls
+# echo CREAT DATABASE >> /tmp/db.sql
+# mysqld --user=mysql --bootstrap --init-file=/tmp/db.sql
+# SHOW DATA BASE
--- mariadb/mariadbconf.sh
+++ mariadb/mariadbconf.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-# mysqld --user=mysql 
-
+echo "CREATE DATABASE nourdin;" >> /tmp/db.sql
+# echo "FLUSH PRIVILEGES;" >> /tmp/db.sql
+cat /tmp/db.sql
+mysqld --user=mysql --bootstrap < /tmp/db.sql
 # SHOW DATA BASE
+exec "$@"
