824a055d26b52be6e0582447cd077b21233cc8ba
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2023-10-04 00:59:16 +0100

    mariadb

--- mariadb/mariadbconf copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ls
-# echo CREAT DATABASE >> /tmp/db.sql
-# mysqld --user=mysql --bootstrap --init-file=/tmp/db.sql
-# SHOW DATA BASE
--- mariadb/mariadbconf.sh
+++ mariadb/mariadbconf.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1001';" >> /tmp/db.sql
 echo "CREATE DATABASE nourdin;" >> /tmp/db.sql
-# echo "FLUSH PRIVILEGES;" >> /tmp/db.sql
+echo "CREATE USER 'nourdine'@'localhost' IDENTIFIED BY 'nourdine';" >> /tmp/db.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'nourdine'@'localhost' WITH GRANT OPTION;" >> /tmp/db.sql
+echo "FLUSH PRIVILEGES;" >> /tmp/db.sql
+# echo "Select user from mysql.user;" >> /tmp/db.sql
+# # echo "mysql -u tester -p" >>  /tmp/db.sql
 cat /tmp/db.sql
 mysqld --user=mysql --bootstrap < /tmp/db.sql
-# SHOW DATA BASE
 exec "$@"
