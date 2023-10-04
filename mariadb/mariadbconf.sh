fec41325242d3fb3c5bab4e5be43b935bc56a93f
nourdine-ouakhroun <nourdineoukhroun@gmail.com>
2023-10-04 20:21:33 +0100

    word press

--- mariadb/mariadbconf.sh
+++ mariadb/mariadbconf.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1001';" >> /tmp/db.sql
+echo "FLUSH PRIVILEGES;" >> /tmp/db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'MyN3wP4ssw0rd';" >> /tmp/db.sql
 echo "CREATE DATABASE nourdin;" >> /tmp/db.sql
 echo "CREATE USER 'nourdine'@'localhost' IDENTIFIED BY 'nourdine';" >> /tmp/db.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO 'nourdine'@'localhost' WITH GRANT OPTION;" >> /tmp/db.sql
 echo "FLUSH PRIVILEGES;" >> /tmp/db.sql
-# echo "Select user from mysql.user;" >> /tmp/db.sql
-# # echo "mysql -u tester -p" >>  /tmp/db.sql
-cat /tmp/db.sql
 mysqld --user=mysql --bootstrap < /tmp/db.sql
 exec "$@"
